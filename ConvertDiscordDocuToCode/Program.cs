19fbf1ca800ebc36d415c46b5bbebf839106ffa6
Cristiano3120 <Cristianocx7@gmail.com>
2025-06-13 13:02:37 +0200

    Projektdateien hinzufügen.

---
+++ ConvertDiscordDocuToCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConvertDiscordDocuToCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
